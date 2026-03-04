7deae09717535cee1863bd9af524fbe80c01287e
Himanshu <himanshupotpose388@gmail.com>
2026-03-04 16:36:38 +0530

    chnagesf

--- RDS-module/database/main.tf
+++ RDS-module/database/main.tf
@@ -4,14 +4,15 @@ resource "random_string" "suffix" {
   special = false
 }
 
-resource "aws_db_instance" "mydb" {
-  identifier           = "${var.project}-${var.env}-${random_string.suffix.result}-mydb"
+resource "aws_db_instance" "db" {
+  identifier           = "${var.project}-${var.env}-db"
   engine               = "mysql"
   instance_class       = "db.t3.micro"
   allocated_storage    = 20
   username             = "admin"
-  password             = himanshu1234
+  password             = "himanshu123"
   db_name              = "mydb"
   skip_final_snapshot  = true
   publicly_accessible  = false
 }
+
