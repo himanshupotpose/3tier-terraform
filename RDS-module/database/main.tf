ab412b5d61dfac62e502fdc8b6a70d556529c96e
Himanshu <himanshupotpose388@gmail.com>
2026-03-04 16:21:17 +0530

    run

--- RDS-module/database/main.tf
+++ RDS-module/database/main.tf
@@ -1,10 +1,17 @@
-resource "aws_db_instance" "db" {
-  identifier = "${var.project}-${var.env}-db"
-  engine = "mysql"
-  instance_class = "db.t3.micro"
-  allocated_storage = 20
-  username = "admin"
-  password = "Password123"
-  skip_final_snapshot = true
-  publicly_accessible = false
+resource "random_string" "suffix" {
+  length  = 4
+  upper   = false
+  special = false
+}
+
+resource "aws_db_instance" "mydb" {
+  identifier           = "${var.project}-${var.env}-${random_string.suffix.result}-mydb"
+  engine               = "mysql"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 20
+  username             = "admin"
+  password             = himanshu1234
+  db_name              = "mydb"
+  skip_final_snapshot  = true
+  publicly_accessible  = false
 }
